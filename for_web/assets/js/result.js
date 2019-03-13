75abea2ad4f92094319c9174a76e8827df445b9e
Uno Lladoc <rubenlladoc@gmail.com>
2019-03-13 15:35:50 +0800

    semi working

--- for_web/assets/js/result.js
+++ for_web/assets/js/result.js
@@ -1,14 +1,20 @@
 angular.module('vote', [])
-.controller('Result', function($scope, $http, $timeout) {
+.controller('Result', function($scope, $http, $timeout,) {
+
+    $scope.i = 0;
 
     $scope.reload = function(){
-        $http.get('http://192.168.1.173:8080/ssgdummyapi/getresult/pres').
+        $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/pres').
             then(function(response) {
                 $scope.pres = response.data;
             });
-        $http.get('http://192.168.1.173:8080/ssgdummyapi/getresult/vp').
+        $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/vp').
+             then(function(response) {
+                 $scope.vpres = response.data;
+             });
+        $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/sen').
             then(function(response) {
-                $scope.vpres = response.data;
+                $scope.sen = response.data;
             });
             
          $timeout(function(){
