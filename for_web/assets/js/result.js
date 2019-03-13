95d5248c1616e1511950a3b2bb14d0abbd38af99
Uno Lladoc <rubenlladoc@gmail.com>
2019-03-13 13:01:19 +0800

    index.html added card and table layout result.js change $scope.disp to $scope.pres -added $scope.vpres -added $scope.reload
    NOTE: For Testing

--- for_web/assets/js/result.js
+++ for_web/assets/js/result.js
@@ -2,6 +2,16 @@ angular.module('vote', [])
 .controller('Result', function($scope, $http) {
     $http.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22').
         then(function(response) {
-            $scope.disp = response.data;
+            $scope.pres = response.data;
         });
+    $http.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22').
+        then(function(response) {
+            $scope.vpres = response.data;
+        });
+        
+    $timeout(function(){
+            $scope.reload();
+          },30000)
+        
+        $scope.reload();
 });
