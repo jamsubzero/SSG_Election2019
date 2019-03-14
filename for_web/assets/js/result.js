c67d8ec7f6aaae1095a5c4210f30f95150f2114f
Uno Lladoc <rubenlladoc@gmail.com>
2019-03-14 11:20:52 +0800

    added itrep

--- for_web/assets/js/result.js
+++ for_web/assets/js/result.js
@@ -1,26 +1,30 @@
 angular.module('vote', [])
-.controller('Result', function($scope, $http, $timeout,) {
+    .controller('Result', function ($scope, $http, $timeout, ) {
 
-    $scope.i = 0;
+        $scope.i = 0;
 
-    $scope.reload = function(){
-        $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/pres').
-            then(function(response) {
-                $scope.pres = response.data;
-            });
-        $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/vp').
-             then(function(response) {
-                 $scope.vpres = response.data;
-             });
-        $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/sen').
-            then(function(response) {
-                $scope.sen = response.data;
-            });
-            
-         $timeout(function(){
-                 $scope.reload();
-             },30000)
-    };        
-             $scope.reload();
-    
-});
+        $scope.reload = function () {
+            $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/pres').
+                then(function (response) {
+                    $scope.pres = response.data;
+                });
+            $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/vp').
+                then(function (response) {
+                    $scope.vpres = response.data;
+                });
+            $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/sen').
+                then(function (response) {
+                    $scope.sen = response.data;
+                });
+            $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/itrep').
+                then(function (response) {
+                    $scope.itrep = response.data;
+                });
+
+            $timeout(function () {
+                $scope.reload();
+            }, 60000)
+        };
+        $scope.reload();
+
+    });
