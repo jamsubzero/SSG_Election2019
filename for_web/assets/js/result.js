2550a653eeb1dc22f5d222249c738d16ab3689e4
Uno Lladoc <rubenlladoc@gmail.com>
2019-03-15 10:23:48 +0800

    chnage vote to votereuslt

--- for_web/assets/js/result.js
+++ for_web/assets/js/result.js
@@ -1,43 +1,43 @@
-angular.module('vote', [])
-    .controller('Result', function ($scope, $http, $timeout, ) {
+angular.module('voteresult', [])
+    .controller('Result', function($scope, $http, $timeout, ) {
 
         $scope.i = 0;
 
-        $scope.reload = function () {
+        $scope.reload = function() {
             $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/pres').
-                then(function (response) {
-                    $scope.pres = response.data;
-                });
+            then(function(response) {
+                $scope.pres = response.data;
+            });
             $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/vp').
-                then(function (response) {
-                    $scope.vpres = response.data;
-                });
+            then(function(response) {
+                $scope.vpres = response.data;
+            });
             $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/sen').
-                then(function (response) {
-                    $scope.sen = response.data;
-                });
+            then(function(response) {
+                $scope.sen = response.data;
+            });
             $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/itrep').
-                then(function (response) {
-                    $scope.itrep = response.data;
-                });
+            then(function(response) {
+                $scope.itrep = response.data;
+            });
             $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/edrep').
-                then(function (response) {
-                    $scope.edrep = response.data;
-                });
+            then(function(response) {
+                $scope.edrep = response.data;
+            });
             $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/barep').
-                then(function (response) {
-                    $scope.barep = response.data;
-                });
+            then(function(response) {
+                $scope.barep = response.data;
+            });
             $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/crrep').
-                then(function (response) {
-                    $scope.crrep = response.data;
-                });
+            then(function(response) {
+                $scope.crrep = response.data;
+            });
             $http.get('https://ssgdummyapi.herokuapp.com/ssgdummyapi/getresult/firep').
-                then(function (response) {
-                    $scope.firep = response.data;
-                });
+            then(function(response) {
+                $scope.firep = response.data;
+            });
 
-            $timeout(function () {
+            $timeout(function() {
                 $scope.reload();
             }, 60000)
         };
